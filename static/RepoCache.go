fc710861dc24842e8e68c809b8c544c499620845
Markus Martin <markus@dynamitedigits.com>
2019-07-26 19:29:15 +0000

    Add license information to the whole project
    Add MIT license and license headers to each file in preparation for open
    source release.

--- static/RepoCache.go
+++ static/RepoCache.go
@@ -1,3 +1,6 @@
+// Copyright 2019 Reaction Engineering International. All rights reserved.
+// Use of this source code is governed by the MIT license in the file LICENSE.txt.
+
 package static
 
 import (
